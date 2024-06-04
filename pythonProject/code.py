91a3f744d19c1ec2749e29464cef69367e72ad08
ethereaxl <115656749+ethereaxl@users.noreply.github.com>
2024-06-04 15:33:27 +0300

    tests

--- pythonProject/code.py
+++ pythonProject/code.py
@@ -1,5 +1,12 @@
 import tkinter as tk
-root = tk.Tk()
-root.title("Snake")
-root.geometry("800x800")
-root.mainloop()
+import random
+class Snake(tk.Canvas):
+    def __init__(self):
+        super().__init__(width=600, height=620, background="black", highlightthickness=0)
+        self.snake_pos = [(100, 100), (80, 100), (60, 100)]
+        self.food_pos = self.set_new_food_position()
+        self.direction = "Right"
+        self.bind_all("<Key>", self.on_key_press)
+        self.load_assets()
+        self.create_objects()
+        self.after(100, self.perform_actions)
